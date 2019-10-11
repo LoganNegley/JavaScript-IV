6d5465c6c731d8527bc55484bd50be82eee0eb03
Logan Negley <logannegley@gmail.com>
2019-10-11 18:08:03 -0500

    created class for student instructor TL

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,15 +1,44 @@
 // CODE here for your Lambda Classes
-class Person = {
-    constructor(attributes){
-        this.name = attributes.name;
-        this.age = attributes.age;
-        this.location = attributes.location;
-    }
-    speak(){
-        return `Hello my name is ${this.name} I am from ${this.location}`;
-    }
+class Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name} I am from ${this.location}`;
+  }
 }
 
-class Instructor extends Person{
-    
+class Instructor extends Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name} I am from ${this.location}`;
+  }
+}
+
+class Student extends Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name} I am from ${this.location}`;
+  }
+}
+
+class TeamLead extends Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name} I am from ${this.location}`;
+  }
 }
