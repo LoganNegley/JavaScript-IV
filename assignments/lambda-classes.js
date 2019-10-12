403adb14ff6889dd32ef60f0c30c7565c282dbdf
Logan Negley <logannegley@gmail.com>
2019-10-11 19:52:35 -0500

    added TL student

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,6 @@
 // CODE here for your Lambda Classes
+
+//Parent constructor
 class Person {
   constructor(attributes) {
     this.name = attributes.name;
@@ -9,36 +11,68 @@ class Person {
     return `Hello my name is ${this.name} I am from ${this.location}`;
   }
 }
-
+//Child of person
 class Instructor extends Person {
-  constructor(attributes) {
-    this.name = attributes.name;
-    this.age = attributes.age;
-    this.location = attributes.location;
+  constructor(instructorAttributes) {
+    super(instructorAttributes);
+    this.specialty = instructorAttributes;
+    this.favLanguage = instructorAttributes;
+    this.catchPhrase = instructorAttributes;
   }
-  speak() {
-    return `Hello my name is ${this.name} I am from ${this.location}`;
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(studentObj, subject) {
+    return `{student.name} receives a perfect score on ${subject}`;
   }
 }
 
+//Child of person
 class Student extends Person {
-  constructor(attributes) {
-    this.name = attributes.name;
-    this.age = attributes.age;
-    this.location = attributes.location;
+  constructor(studentAttributes) {
+    super(studentAttributes);
+    this.previousBackground = studentAttributes.previousBackground;
+    this.className = studentAttributes.className;
+    this.favSubjects = studentAttributes.favSubjects;
   }
-  speak() {
-    return `Hello my name is ${this.name} I am from ${this.location}`;
+  listsSubjects() {
+    return this.favSubjects;
+  }
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+}
+//Grandchild of person- child of instructor
+class TeamLead extends Instructor {
+  constructor(tlAttributes) {
+    super(tlAttributes);
+    this.gradClassName = tlAttributes.gradClassName;
+    this.favInstructor = tlAttributes.favInstructor;
+  }
+  standUp(channel) {
+    `${this.name} announces to ${channel} time for standup! `;
+  }
+
+  debugCode(studentObj, subject) {
+    return `${this.name} debugs ${studentObj.name} code on ${subject}`;
   }
 }
 
-class TeamLead extends Person {
-  constructor(attributes) {
-    this.name = attributes.name;
-    this.age = attributes.age;
-    this.location = attributes.location;
-  }
-  speak() {
-    return `Hello my name is ${this.name} I am from ${this.location}`;
-  }
-}
+const leader = new TeamLead({
+  name: "Joey",
+  age: 56,
+  location: "Cali",
+  gradClassName: "Computer Science",
+  favInstructor: "Josh Knoll"
+});
+
+const student1 = new Student({
+  name: "Logan",
+  age: 30,
+  location: "Texas",
+  previouseBackground: "EMT",
+  className: "WEBPT11",
+  favSubject: ["CSS", "HTML", "JS"]
+});
+
+console.log(leader.debugCode(student1, "CSS"));
